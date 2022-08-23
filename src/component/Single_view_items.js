905678ae6cce7750e3b14d14d1391de548f8e981
rajonhossain <rajonhossaindhaka@gmail.com>
2022-08-23 13:59:01 +0600

    tas
    ads

--- src/component/Single_view_items.js
+++ src/component/Single_view_items.js
@@ -79,8 +79,13 @@ const Single_view_items = () => {
     const decrement = () => {
         var data = JSON.parse(localStorage.getItem("add_items") || "[]");
         var index = data.findIndex(x => x.item_name === single_items_data[0].item_name);
-        if (data[index].qnt != 1) {
+        if (data[index].qnt < 1) {
+            var index = data.findIndex(x => x.item_name === single_items_data[0].item_name);        
+            data.splice(index, 1);
+            localStorage.setItem('add_items', JSON.stringify(data));
+        } else {
             data[index].qnt = data[index].qnt - 1;
+
         }
         localStorage.setItem("add_items", JSON.stringify(data));
     }
